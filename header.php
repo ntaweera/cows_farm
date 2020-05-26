bf036774392f2435918ff710a0b2feb7c8398080
RNCAT <xshootercast@gmail.com>
2020-05-26 12:05:27 +0700

    Add kanit font

--- header.php
+++ header.php
@@ -4,9 +4,16 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Font Kanit -->
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
+    
+    <style>
+      * {
+        font-family: 'Kanit', sans-serif;
+      }
+    </style>
 
     <title>ระบบฐานข้อมูลวัวขุนอำเภอคลองหอยโข่ง</title>
   </head>
