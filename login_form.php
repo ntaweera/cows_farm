e59da14bf8e35801ee644aba75ec02f1df27acc8
RNCAT <xshootercast@gmail.com>
2020-05-26 15:19:11 +0700

    nav-item to right

--- login_form.php
+++ login_form.php
@@ -1,19 +1,19 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   
-  <button class="navbar-toggler" type="button" 
-  data-toggle="collapse" data-target="#navbarSupportedContent" 
-  aria-controls="navbarSupportedContent" aria-expanded="false" 
-  aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" 
+          data-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" aria-expanded="false" 
+          aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon navbar-expand-*"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
        <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="ชื่อผู้ใช้" >
-        <input type="password" name="password" placeholder="รหัสผ่าน">
+        <input type="text" name="username" placeholder="ชื่อผู้ใช้" required>
+        <input type="password" name="password" placeholder="รหัสผ่าน" required>
         <input type="submit" value="ลงชื่อเข้าใช้" class="btn btn-info">
        </form>
       </li>
